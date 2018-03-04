f088001431fce14d598511a20ed747197c6441f1
370980250 <370980250@qq.com>
2018-03-05 00:04:46 +0800

    nba

--- nba2kx/src/main/java/com/tx/nba2kx/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.tx.nba2kx.dao;
-
-import org.apache.ibatis.annotations.Insert;
-
-public interface UserDao {
-    @Insert("insert into tx_user ")
-    int addUser();
-}
--- tx_nba2k/src/com/tx/service/inter/IPowerService.java
+++
@@ -1,9 +0,0 @@
-package com.tx.service.inter;
-
-import java.util.List;
-
-import com.tx.pojo.Power;
-
-public interface IPowerService {
-	public List<Power> queryPowerList();
-}
